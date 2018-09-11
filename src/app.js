0f7133c489fbb85de321c51e5ec647a0bc87ff75
xwang3000 <xwang3000@gmail.com>
2018-09-11 18:34:45 +0800

    Create item list component.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
-var app = new Vue({
-  el: '#app',
+// <item>
+Vue.component('item', {
+  props: [
+    'item'
+  ],
+  template: `
+  <li class="item">
+    <div class="item-fav-icon"> {{ item.isFav ? "😊" : "☹️" }} </div>
+    <img class="item-image" v-bind:src="item.imageSrc" alt="">
+    <div> {{ item.brand }} </div>
+    <div class="item-color"> {{ item.color }} </div>
+  </li>
+  `
+})
+
+// <items>
+
+Vue.component('items', {
   template: `
     <ul class="item-list">
       <item 
@@ -12,16 +28,18 @@ var app = new Vue({
       </item>
     </ul>
     `,
-    data: {
-    message: 'Buy a new purse',
-    purses: [
-      { id: 0, brand: 'Bao Bao', color: 'Classic Silver', imageSrc: '/assets/issey.jpeg', isFav: true },
-      { id: 1, brand: 'Longchamp', color: 'Grass Khaki', imageSrc: '/assets/longchamp.jpeg', isFav: false },
-      { id: 2, brand: 'Coach', color: 'Lychee Black', imageSrc: '/assets/coach.jpeg', isFav: true },
-      { id: 3, brand: 'Louis Vuitton', color: 'Shiny Nude', imageSrc: '/assets/alma.jpeg', isFav: true },
-      { id: 4, brand: 'Chanel', color: 'Black Fabric', imageSrc: '/assets/leboy.png', isFav: false },
-      { id: 5, brand: 'Louis Vuitton', color: 'Pink', imageSrc: '/assets/girolata.png', isFav: false }
-    ]
+    data: function () {
+      return {
+        message : 'Buy a new purse',
+        purses : [
+          { id: 0, brand: 'Bao Bao', color: 'Classic Silver', imageSrc: '/assets/issey.jpeg', isFav: true },
+          { id: 1, brand: 'Longchamp', color: 'Grass Khaki', imageSrc: '/assets/longchamp.jpeg', isFav: false },
+          { id: 2, brand: 'Coach', color: 'Lychee Black', imageSrc: '/assets/coach.jpeg', isFav: true },
+          { id: 3, brand: 'Louis Vuitton', color: 'Shiny Nude', imageSrc: '/assets/alma.jpeg', isFav: true },
+          { id: 4, brand: 'Chanel', color: 'Black Fabric', imageSrc: '/assets/leboy.png', isFav: false },
+          { id: 5, brand: 'Louis Vuitton', color: 'Pink', imageSrc: '/assets/girolata.png', isFav: false }
+        ]
+      }
   },
   computed: {
     reversedMessage: function () {
@@ -38,17 +56,16 @@ var app = new Vue({
   }
 
 })
-// Components
-Vue.component('item', {
-  props: [
-    'item'
-  ],
-  template: `
-  <li class="item">
-    <div class="item-fav-icon"> {{ item.isFav ? "😊" : "☹️" }} </div>
-    <img class="item-image" v-bind:src="item.imageSrc" alt="">
-    <div> {{ item.brand }} </div>
-    <div class="item-color"> {{ item.color }} </div>
-  </li>
+
+// <app>
+
+var app = new Vue({
+  el: '#app',
+  template: 
+  `
+    <div>
+      <p>PURSES</p>
+      <items></items>
+    </div>
   `
 })
