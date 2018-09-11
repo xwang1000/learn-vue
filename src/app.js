02b207e643ecd2aea5682bf3157d801f5e1e813b
xwang3000 <xwang3000@gmail.com>
2018-09-11 19:33:59 +0800

    Implement toggleFav function.

--- src/app.js
+++ src/app.js
@@ -5,10 +5,14 @@ Vue.component('item', {
     item: {
       required: true,
       type: Object
+    },
+    clicked: {
+      required: true,
+      type: Function
     }
   },
   template: `
-    <li class="item">
+    <li class="item" @click="clicked(item.id)">
       <div class="item-fav-icon">{{ item.isFav ? "😊" : "☹️" }}</div>
       <img class="item-image" v-bind:src="item.imageSrc" alt="">
       <div>{{ item.brand }}</div>
@@ -26,23 +30,31 @@ Vue.component('items', {
         class="item"
         v-for="purse in purses"
         :item="purse"
+        :clicked="toggleFav"
         :key="purse.id"
-        @click="toggleFav()"
       >
       </item>
     </ul>
   `,
   data () {
     return {
-      message: 'Buy a new purse',
       purses: [
-        { id: 0, brand: 'Bao Bao', color: 'Classic Silver', imageSrc: '/assets/issey.jpeg', isFav: true },
+        { id: 0, brand: 'Bao Bao', color: 'Classic Silver', imageSrc: '/assets/issey.jpeg', isFav: true},
         { id: 1, brand: 'Longchamp', color: 'Grass Khaki', imageSrc: '/assets/longchamp.jpeg', isFav: false },
         { id: 2, brand: 'Coach', color: 'Lychee Black', imageSrc: '/assets/coach.jpeg', isFav: true },
         { id: 3, brand: 'Louis Vuitton', color: 'Shiny Nude', imageSrc: '/assets/alma.jpeg', isFav: true },
         { id: 4, brand: 'Chanel', color: 'Black Fabric', imageSrc: '/assets/leboy.png', isFav: false },
         { id: 5, brand: 'Louis Vuitton', color: 'Pink', imageSrc: '/assets/girolata.png', isFav: false }
       ]
+    }
+  },
+  methods: {
+    toggleFav (ID) {
+      this.purses.forEach((purse) => {
+        if (purse.id === ID) {
+          purse.isFav = !purse.isFav
+        }
+      })
     }
   }
 })
